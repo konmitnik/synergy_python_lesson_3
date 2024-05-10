7e7082cdee41cd67003a49b94731955103a8eab5
konmitnik <konmitnik@gmail.com>
2024-05-10 16:54:50 +0300

    push files

---
+++ task1.py
@@ -0,0 +1,5 @@
+animal_type = input("Введите вид животного: ")
+animal_age = input("Введите, сколько лет животному: ")
+animal_name = input("Введите кличку животного: ")
+
+print(f"Это {animal_type} по кличке \"{animal_name}\". Возраст: {animal_age}")
---
+++ task2.py
@@ -0,0 +1,7 @@
+australopithecus = input("первая ступень развития рода Homo: ")
+habilis = input("вторая ступень: ")
+erectus = input("третья ступень: ")
+neanderthalensis = input("четвертая ступень: ")
+sapiens = input("пятая ступень: ")
+
+print(australopithecus, habilis, erectus, neanderthalensis, sapiens, sep="=>")
